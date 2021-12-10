158180a78399016392e9d145152948a465bfc08d
dereck <lordandre8@gmail.com>
2021-12-10 20:25:05 +0000

    first commit

---
+++ app/Stagiaire.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stagiaire extends Model
+{
+    //use HasFactory;
+}
